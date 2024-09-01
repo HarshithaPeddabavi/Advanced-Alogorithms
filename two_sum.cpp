85992cabbc6a567833b9bf4083d7db7050cbc4ae
HarshithaPeddabavi <harshithapeddabavi@gmail.com>
2024-08-31 22:58:25 -0500

    Update two_sum.cpp
    updated the code

--- two_sum.cpp
+++ two_sum.cpp
@@ -4,18 +4,18 @@
 
 using namespace std;
 
-// Function to find the indices of two numbers that add up to the target
+// Function for determining the indices of two numbers that add up to the target.
 
 vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> numMap; // Hash map to store number and its index
+    unordered_map<int, int> numMap; // Hash map for storing numbers and their indexes
 
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i]; // Find the complement of the current number
         if (numMap.find(complement) != numMap.end()) {
-            // Return the indices if the complement is found in the map
+            // Return the indices if the complement exists in the map
             return {numMap[complement], i};
         }
-        // Store the current number and its index in the map
+        // Record the current number and its index in the map
         numMap[nums[i]] = i;
     }
     
@@ -23,10 +23,10 @@ vector<int> twoSum(vector<int>& nums, int target) {
 }
 
 int main() {
-    vector<int> nums = {2, 7, 11, 15}; // input
-    int target = 9; // target
+    vector<int> nums = {3,2,4}; // input
+    int target = 6; // target
     
-    // Get the indices from the twoSum function
+    // Retrieve the indices from the twoSum function
     vector<int> result = twoSum(nums, target);
     
     // Print the indices
